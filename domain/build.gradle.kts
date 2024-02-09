62d3a9f337913722188d0158bc907eaa4d2cf23e
jonathan <ghkddlsrb96@gmail.com>
2024-02-09 16:15:00 +0900

    테스트 의존성 추가 모의 객체를 위한 mockk 라이브러리 추가 코루틴 추가 common 도메인 모듈(dto, regex check 등 util이나 공통적인 기능을 담은 모듈) 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,20 @@
 plugins {
+    kotlin("kapt")
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+val mockkVersion = "1.13.9"
+val coroutineVersion = "1.8.0-RC2"
+dependencies {
+    implementation(project(":common"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:${mockkVersion}")
 }
