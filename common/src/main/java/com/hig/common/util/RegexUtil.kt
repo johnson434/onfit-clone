869eeaba0529b8f8d20ad8c1d833492e30df10ed
jonathan <ghkddlsrb96@gmail.com>
2024-02-13 23:49:03 +0900

    UserName 정규식 추가 한글, 영어, 숫자로 이루어진 2글자 이상 10글자 이하

--- common/src/main/java/com/hig/common/util/RegexUtil.kt
+++ common/src/main/java/com/hig/common/util/RegexUtil.kt
@@ -10,4 +10,9 @@ object RegexUtil {
         val emailPattern = Regex("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$")
         return emailPattern.matches(email)
     }
+
+    fun isValidUserName(userName: String): Boolean {
+        val userNamePattern = Regex("[a-zA-zㄱ-ㅎ가-힣0-9]{2,10}")
+        return userNamePattern.matches(userName)
+    }
 }
